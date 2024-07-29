f00a7f030f87926528fdd92d0905d0cf9a4ef3a8
sebastiandion <swd7104@gmail.com>
2024-07-29 10:26:12 -0400

    Set up Structs

--- main.cpp
+++ main.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
 #include "include/json.hpp"
+using namespace std;
+
+struct Song{
+    string id;
+};
+
+struct Artist{
+    string name;
+    double familiarity;
+    string terms;
+};
+
+struct Node {
+    Song song;
+    Artist artist;
+    vector<Node*> neighbors;
+};
+
+class Graph{
+private:
+    unordered_map<string, Node> nodes;
+public:
+
+};
+
+
 
 
 int main() {
